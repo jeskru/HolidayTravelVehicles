a38cce24c60d60cd95c5e726e0a422b8d0f2a5dc
jeskru <jeskru@uni-bremen.de>
2025-09-21 20:56:40 +0700

    Create Invoice.java

---
+++ Invoice.java
@@ -0,0 +1,12 @@
+public class Invoice {
+    String InvoiceId;
+    int Finalprice;
+    Customer customer;
+    SalesPerson salesPerson;
+    NewVehicle newVehicle;
+    TradeInVehicle tradeInVehicle;
+    Option option;
+    int tax;
+    int licenseFee;
+    Date date;
+}
